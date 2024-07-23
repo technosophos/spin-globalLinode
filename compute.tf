080508fd83d903d1726c47b4005a4ab4cd85ec60
Matt Butcher <matt.butcher@fermyon.com>
2024-07-23 09:33:04 -0600

    Initial add of Spin changes
    Signed-off-by: Matt Butcher <matt.butcher@fermyon.com>

--- compute.tf
+++ compute.tf
@@ -1,14 +1,14 @@
-resource "linode_instance" "neuralmagic-primary" {
+resource "linode_instance" "fermyon-primary" {
   depends_on = [data.http.regions]  # Make sure that the Linode instances are created after the HTTP request
 
   count = length(jsondecode(data.http.regions.response_body).data)
 
-  label      = "deepsparse-${jsondecode(data.http.regions.response_body).data[count.index].id}"
+  label      = "spin-${jsondecode(data.http.regions.response_body).data[count.index].id}"
   image      = "linode/ubuntu22.04"
   region     = jsondecode(data.http.regions.response_body).data[count.index].id
   type       = "g6-dedicated-2"
-  root_pass  = "Terr4form-test!" # Change this!
-  tags       = ["app:sentimentanalysis"]
+  root_pass  = "Terr4form-Sp1n-test!" # Change this!
+  tags       = ["app:spin"]
 
   metadata {
     user_data = base64encode(file("./linode.yaml"))
